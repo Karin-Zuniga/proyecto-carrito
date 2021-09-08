104ed2e7e5b157be12ccccf23560aa1a41733d6b
Karin-Zuniga <karin.zuniga.munoz@gmail.com>
2021-09-08 18:54:03 -0300

    cambio en deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "carrito"
 set :repo_url, "git@github.com:Karin-Zuniga/proyecto-carrito.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git rev-parse --abbrev-ref MAIN`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/ubuntu/#{fetch :application}"
